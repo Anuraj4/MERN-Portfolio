52ec1c1d001d7bd9c6ae26b66144dd7899e64473
Anuraj Venkatpurwar <anurajvenkatpurwar@gmail.com>
2024-10-14 12:20:02 +0530

    Adding onrender api on frontend

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -24,7 +24,7 @@ const Contact = () => {
 
         try {
             // Send form data to backend
-            const response = await fetch('http://localhost:5000/api/contact', {
+            const response = await fetch('https://mern-portfolio-3x0o.onrender.com', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
